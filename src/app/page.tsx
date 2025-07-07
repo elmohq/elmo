edd43bd5ee12bb56cc70886bf275fa6ee666cdf5
jrhizor <me@jaredrhizor.com>
2025-07-07 12:44:20 -0700

    dashboard outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import { db } from "@/lib/db/db";
 import Profile from "@/components/profile";
 import { WHITE_LABEL_CONFIG } from "@/lib/white-label";
 
-export default async function Home() {
-
-  const result = await db.execute('select 1');
-
+export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {WHITE_LABEL_CONFIG.name}
+    <div className="w-full">
+      <main className="w-full">
         <Profile />
       </main>
     </div>
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,13 +1,42 @@
 'use client';
 import { useUser } from "@auth0/nextjs-auth0"
+import { SidebarInset, SidebarProvider } from "./ui/sidebar";
+import { AppSidebar } from "./app-sidebar";
+import { SiteHeader } from "./site-header";
+import { SectionCards } from "./section-cards";
+import { WHITE_LABEL_CONFIG } from "@/lib/white-label";
 
 export default function Profile() {
   const { user, isLoading } = useUser();
-  return (
-    <>
-      {isLoading && <p>Loading...</p>}
-      {user && (
-        <div style={{ textAlign: "center" }}>
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!user) {
+    return <a href="/auth/login">Login</a>;
+  }
+
+  if (user) {
+    return  <SidebarProvider
+    className="flex"
+    style={
+      {
+        "--sidebar-width": "calc(var(--spacing) * 64)",
+        "--header-height": "calc(var(--spacing) * 12 + 1px)",
+      } as React.CSSProperties
+    }
+  >
+    <AppSidebar variant="sidebar" />
+    <SidebarInset>
+      <SiteHeader />
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+
+        {WHITE_LABEL_CONFIG.name}
+            <div style={{ textAlign: "center" }}>
           <img
             src={user.picture}
             alt="Profile"
@@ -18,8 +47,10 @@ export default function Profile() {
           <pre>{JSON.stringify(user, null, 2)}</pre>
           <p><a href="/auth/logout">Logout</a></p>
         </div>
-      )}
-      {!user && <a href="/auth/login">Login</a>}
-    </>
-  );
+          </div>
+        </div>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>;
+  }
 }
