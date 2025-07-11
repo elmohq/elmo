a84ad59e1355ccf5ad6510078eb15d3a003113cc
jrhizor <me@jaredrhizor.com>
2025-07-11 16:20:44 -0700

    update name to AEO

--- src/lib/white-label.ts
+++ src/lib/white-label.ts
@@ -1,7 +1,7 @@
 export const WHITE_LABEL_CONFIG = {
-	name: "Fermàt GEO",
+	name: "Fermàt AEO",
 	icon: "/brands/whitelabel-client/icon.png",
-	url: "https://geo.whitelabel-client.com/",
+	url: "https://aeo.whitelabel-client.com/",
 	parent_name: "Fermàt",
 	parent_url: "https://app.whitelabel-client.com/",
 };
