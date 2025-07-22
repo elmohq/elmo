7076fa1f60cf42fb51c4e16e8440cf16e31af102
jrhizor <me@jaredrhizor.com>
2025-07-21 18:38:42 -0700

    remove debug page, components, and unsafe endpoints

--- src/app/debug/brands-debug.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useBrands } from "@/hooks/use-brands";
-
-export default function BrandsDebug() {
-	const brands = useBrands();
-
-	return <pre>{JSON.stringify(brands, null, 2)}</pre>;
-}
