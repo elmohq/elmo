f21d873ee8627013a61ce6bf613de289aace2dd3
jrhizor <me@jaredrhizor.com>
2025-09-06 12:27:29 -0700

    format

--- apps/web/placeholder.test.ts
+++ apps/web/placeholder.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from "vitest";
 
-describe('Placeholder Test', () => {
-  it('temporarily pass build', () => {
-    expect(true).toBe(true)
-  })
-})
+describe("Placeholder Test", () => {
+  it("temporarily pass build", () => {
+    expect(true).toBe(true);
+  });
+});
