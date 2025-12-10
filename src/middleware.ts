7e7929006d3aac3708913a7dda3622a979ffdb7c
jrhizor <me@jaredrhizor.com>
2025-12-09 19:58:43 -0800

    additional protection for the frontend side of the admin page

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/app/:path*", "/reports/:path*", "/auth/:path*", "/api/:path*"],
+	matcher: ["/app/:path*", "/admin/:path*", "/reports/:path*", "/auth/:path*", "/api/:path*"],
 };
