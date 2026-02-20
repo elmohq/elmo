68b3ff71c5c45f322e4bbadb5ad68b9e211f1939
jrhizor <me@jaredrhizor.com>
2026-02-20 00:46:18 -0800

    change where sentry is initialized

--- apps/web/src/server.ts
+++ apps/web/src/server.ts
@@ -1,8 +1,16 @@
-import { wrapFetchWithSentry } from "@sentry/tanstackstart-react";
+import * as Sentry from "@sentry/tanstackstart-react";
 import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
 
+if (process.env.SENTRY_DSN) {
+	Sentry.init({
+		dsn: process.env.SENTRY_DSN,
+		sendDefaultPii: true,
+		tracesSampleRate: 1.0,
+	});
+}
+
 export default createServerEntry(
-	wrapFetchWithSentry({
+	Sentry.wrapFetchWithSentry({
 		fetch(request: Request) {
 			return handler.fetch(request);
 		},
