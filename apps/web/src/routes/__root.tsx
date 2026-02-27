edf7f604fc6d175b0e463bbba51094be0ef3139f
jrhizor <me@jaredrhizor.com>
2026-02-26 17:40:27 -0800

    fix clarity script

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -49,8 +49,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 		}
 		if (analytics?.clarityProjectId) {
 			scripts.push({
-				src: `https://www.clarity.ms/tag/${analytics.clarityProjectId}`,
-				defer: true,
+				children: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","${analytics.clarityProjectId}");`,
 			});
 		}
 
